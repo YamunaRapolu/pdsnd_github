4791ae9682bea341bf395b5782daff67276b2354
root <root@localhost>
2025-12-13 12:52:03 +0000

    Refactor: improve readability using greeting variable

--- bikeshare.py
+++ bikeshare.py
@@ -3,7 +3,10 @@ Bikeshare Project
 This script analyzes bikeshare data.
 """
 def main():
-    print("Welcome to the Bikeshare Project")
+    """Main function"""
+    print("Welcome to the Bikeshare Project!")
+    print("Data analysis will begin shortly.")
+
 if __name__=="__main__":
      main()
 
