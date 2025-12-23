b704cab14dbd67fad18dc65789670d606706b86e
root <root@localhost>
2025-12-23 13:52:10 +0000

    Changes are done

--- bikeshare.py
+++ bikeshare.py
@@ -1,10 +1,11 @@
+
 """
 The Bikeshare Project
 This script analyzes bikeshare data.
 """
 def main():
     """Main function"""
-    print("Welcome to the Bikeshare Project!")
+   
     print("Data analysis will begin shortly.")
 
 if __name__=="__main__":
